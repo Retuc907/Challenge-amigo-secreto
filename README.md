6bdf1627e0fd26ae7c259b2641d18d99be6a9af2
retuc <sabu.proyectos@gmail.com>
2025-02-13 08:50:30 -0500

    aplicacion finalizada

---
+++ README.md
@@ -0,0 +1,22 @@
+# 🎉 Amigo Secreto
+
+Una aplicación simple para organizar un sorteo de "Amigo Secreto". Permite agregar amigos a una lista y realizar un sorteo aleatorio para asignar parejas de intercambio.
+
+## 🚀 Características
+
+- Agregar nombres a una lista de amigos.
+- Mostrar la lista de participantes.
+- Sortear un amigo secreto de forma aleatoria.
+- Interfaz sencilla y fácil de usar.
+
+## 🛠️ Tecnologías utilizadas
+
+- HTML
+- CSS
+- JavaScript (sin frameworks)
+
+## 📦 Instalación y uso
+
+1. **Clona el repositorio**  
+   ```sh
+   git clone https://github.com/tu-usuario/amigo-secreto.git
--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+console.log("El script se ha cargado correctamente.");
+
 let listaDeAmigos = [];
+
 
 
 
@@ -11,10 +14,38 @@ function agregarAmigo() {
     }
     else {
         listaDeAmigos.push(ingresarNombre);
-        console.log(listaDeAmigos);
             document.querySelector('#amigo').value='';
+            mostrarAmigos();  
         }
+}
+
+
+
+function mostrarAmigos (){
+    let mostrarLista = document.getElementById ('listaAmigos');
+    mostrarLista.innerHTML = '';
+
+    for (let i=0; i<listaDeAmigos.length; i++){
+        mostrarLista.innerHTML += `<li>${listaDeAmigos[i]}</li>`;
     }
 
-agregarAmigo();
+    let resultadoLista = document.getElementById('resultado');
+    resultadoLista.innerHTML = `Total de amigos: ${listaDeAmigos.length}`;
+}
+
+function sortearAmigoSecreto(){
+    console.log("El botón fue presionado.");
+    if (listaDeAmigos.length === 0) {
+        alert('No hay amigos en la lista para sortear');
+        return;
+    }
+
+   let amigoAleatorio = listaDeAmigos[Math.floor(Math.random()*listaDeAmigos.length)];
+    alert(`Tu amigo secreto es: ${amigoAleatorio}`);
+    document.getElementById('amigo').value='';
+    document.getElementById('listaAmigos').innerHTML = '';
+    document.getElementById('resultado').innerHTML = '';
+}    
+
+
 
